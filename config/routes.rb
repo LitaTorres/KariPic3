19a0fc83b9b70fe8c50fb440bb4d0b04b8c0f73e
Leslie Torres <Leslietorresc@gmail.com>
2023-09-10 19:54:44 -0300

    commit scaffold devise active storage

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  resources :comments
+  resources :publications
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
+  
+  get 'homes/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+   root "homes#index"
+   get "/publicatons", to: "publications#index", as: "user_root"
+end
